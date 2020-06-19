8bdac795ab10477cc5b9b63c595da7d7d357e18c
Joshua Trimm <jbthype@gmail.com>
2020-06-19 10:30:49 -0400

    Starting Database

---
+++ Models/HashTags.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RipMyPaperToShreds.com.Models
+{
+    public class HashTags
+    {
+        public int ID { get; set; }
+        public string HashTag { get; set; }
+    }
+}
